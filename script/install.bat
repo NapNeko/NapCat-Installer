fa0e2b129051a517427fdcc7a6fb94377fd29f5e
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-27 17:26:06 +0800

    flush

--- script/install.bat
+++ script/install.bat
@@ -1,3 +1,3 @@
-curl -o install.ps1 https://fastly.jsdelivr.net/gh/NapNeko/NapCat-Installer@master/script/install.ps1
+curl -o install.ps1 https://fastly.jsdelivr.net/gh/NapNeko/NapCat-Installer@main/script/install.ps1
 Net session >nul 2>&1
 powershell -ExecutionPolicy RemoteSigned -File ./install.ps1 -verb runas
