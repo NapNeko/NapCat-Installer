1f3df49036fe601a4b3e07aa19f8bd1f325918fe
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-15 10:17:19 +0800

    Update install.bat

--- script/install.bat
+++ script/install.bat
@@ -1,2 +1,3 @@
 curl -o install.ps1 https://nclatest.znin.net/NapNeko/NapCat-Installer/main/script/install.ps1
+taskkill /f /im QQ.exe
 powershell -ExecutionPolicy ByPass -File ./install.ps1 -verb runas
