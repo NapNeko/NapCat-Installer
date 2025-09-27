58a93456a64cb1d73e45360d6436be82f943ff58
flyqie <flyqie@outlook.com>
2025-09-27 22:57:27 +0800

    fix: rootless update

--- script/install.termux.sh
+++ script/install.termux.sh
@@ -36,7 +36,7 @@ execute_command "proot-distro install debian --override-alias napcat" "安装nap
 
 echo -e "${GREEN}正在初始化napcat容器...${NC}"
 init_cmd="apt update -y && \
-apt install -y sudo curl && \
+apt install -y sudo curl libgcrypt20 && \
 curl -o napcat.sh https://nclatest.znin.net/NapNeko/NapCat-Installer/main/script/install.sh && \
 sudo bash napcat.sh --docker n --cli n && \
 apt autoremove -y && \
@@ -48,13 +48,13 @@ if [ $? -ne 0 ]; then
     echo -e "${RED}napcat容器初始化失败。${NC}"
     exit 1
 fi
-echo -e "${RED}napcat容器安装成功, 上方无效信息无需关注, 请参考下方说明⬇${NC}"
+echo -e "${RED}napcat容器安装成功, 上方信息无需关注, 请参考下方说明⬇${NC}"
 
-echo -e "\n安装完成, 请输入${GREEN} proot-distro sh napcat -- bash -c \"xvfb-run -a qq --no-sandbox\" ${NC}命令启动。"
-echo -e "保持后台运行 请输入${GREEN} screen -dmS napcat bash -c 'proot-distro sh napcat -- bash -c \"xvfb-run -a qq --no-sandbox\"'${NC}"
-echo -e "后台快速登录 请输入${GREEN} screen -dmS napcat bash -c 'proot-distro sh napcat -- bash -c \"xvfb-run -a qq --no-sandbox -q QQ号码\"'${NC}"
+echo -e "\n安装完成, 请输入${GREEN} proot-distro sh napcat -- bash -c \"xvfb-run -a /root/Napcat/opt/QQ/qq --no-sandbox\" ${NC}命令启动。"
+echo -e "保持后台运行 请输入${GREEN} screen -dmS napcat bash -c 'proot-distro sh napcat -- bash -c \"xvfb-run -a /root/Napcat/opt/QQ/qq --no-sandbox\"'${NC}"
+echo -e "后台快速登录 请输入${GREEN} screen -dmS napcat bash -c 'proot-distro sh napcat -- bash -c \"xvfb-run -a /root/Napcat/opt/QQ/qq --no-sandbox -q QQ号码\"'${NC}"
 echo -e "进入容器内部 请输入${GREEN} proot-distro login napcat ${NC}"
 echo -e "容器数据位置${MAGENTA} /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/napcat${NC}"
-echo -e "Napcat安装位置(容器外真实路径)${MAGENTA} /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/napcat/opt/QQ/resources/app/app_launcher/napcat${NC}"
+echo -e "Napcat安装位置(容器外真实路径)${MAGENTA} /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/napcat/root/Napcat/opt/QQ/resources/app/app_launcher/napcat${NC}"
 echo -e "注意, 您可以随时使用${GREEN}screen -r napcat${NC}来进入后台进程并使用${GREEN}ctrl + a + d${NC}离开(离开不会关闭后台进程)。"
-echo -e "${GREEN}WEB_UI访问密钥在上方信息中${NC}"
+echo -e "${GREEN}WEB_UI访问密钥请查看 Napcat安装位置/config/webui.json ${NC}"
